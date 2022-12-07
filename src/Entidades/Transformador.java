cc471f8a243d8e5d9e1f7fd7bf3726ecd7717d3b
Vitor Santos <vitoralannl@gmail.com>
2022-12-07 11:49:37 -0300

    refactor (Transformador) - Refatorando código de transformação

--- src/Entidades/Transformador.java
+++ src/Entidades/Transformador.java
@@ -17,17 +17,7 @@ public class Transformador {
 
     //não finalizado
     public void toAFD(Automato automato) {
-        List<Transicao> transicoes = automato.getTransicoes();
-        Iterator<Transicao> iterator = transicoes.iterator();
-
-        List<List<Estado>> lista = new ArrayList<>();
-
-        String arr[] = new String[automato.getEstados().size()];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = automato.getEstados().get(i).getNome();
-        }
-        int n = arr.length;
+        int n = automato.getEstados().size();
 
         Estado[] estados = new Estado[automato.getEstados().size()];
 
@@ -78,7 +68,6 @@ public class Transformador {
 
     void combinationUtil(Estado arr[], Estado data[], int start,
                          int end, int index, int r) {
-        // Current combination is ready to be printed, print it
         if (index == r) {
             List<Estado> estados = new ArrayList<>();
             for (int j = 0; j < r; j++) {
@@ -88,23 +77,15 @@ public class Transformador {
             return;
         }
 
-        // replace index with all possible elements. The condition
-        // "end-i+1 >= r-index" makes sure that including one element
-        // at index will make a combination with remaining elements
-        // at remaining positions
         for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
             data[index] = arr[i];
             combinationUtil(arr, data, i + 1, end, index + 1, r);
         }
     }
 
-    // The main function that prints all combinations of size r
-    // in arr[] of size n. This function mainly uses combinationUtil()
     void printCombination(Estado arr[], int n, int r) {
-        // A temporary array to store all combination one by one
         Estado data[] = new Estado[r];
 
-        // Print all combination using temporary array 'data[]'
         combinationUtil(arr, data, 0, n - 1, 0, r);
     }
 
